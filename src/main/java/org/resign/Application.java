35ca952c7bd447632ab127f583f1c57888b4f4bb
cmitsoftware <cmit.software@gmail.com>
2018-05-31 11:58:28 +0200

    added listener to write pid

--- src/main/java/org/resign/Application.java
+++ src/main/java/org/resign/Application.java
@@ -2,11 +2,14 @@ package org.resign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+		SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());     // register PID write to spring boot. It will write PID to file
+        springApplication.run(args);
     }
 }
